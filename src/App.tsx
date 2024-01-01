d4be7e464f6293cf820357499a72b97544b67832
Onur Şenel <onrsenel@gmail.com>
2024-01-02 02:27:23 +0300

    homepage düzenlendi ve route kullanıldı

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { ReactElement } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage/Homepage";
 
 
 
 
 function App():ReactElement {
-  return (
-    <div></div>
+  return (<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Homepage></Homepage>}></Route>
+  </Routes>
+  </BrowserRouter>
 
   );
 }
---
+++ src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,17 @@
+
+
+type Props = {}
+
+const Homepage = (props: Props) => {
+  return (
+    <div className="container">
+        <div className="row">
+            <div className="col-3">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Homepage
