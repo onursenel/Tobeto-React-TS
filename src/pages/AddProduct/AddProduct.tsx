8acaeb7a1319320d2c551f4c200753c4afc70da0
Onur Şenel <onrsenel@gmail.com>
2024-01-02 15:39:22 +0300

    formik içerisinde input tekrarlı hali

--- src/pages/AddProduct/AddProduct.tsx
+++ src/pages/AddProduct/AddProduct.tsx
@@ -1,5 +1,6 @@
-import { Field, Form, Formik } from 'formik'
-import React from 'react'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+
+import { number, object, string } from 'yup';
 
 type Props = {};
 
@@ -14,24 +15,41 @@ const AddProduct = (props: Props) => {
         stock: 0,
     }
 
+    const validationSchema = object({
+        title: string().required("Başlık alanı zorunludur").min(3,"Başlık alanı en az 3 karakter olmalıdır!").max(50),
+        description: string().required("ürün açıklaması zorunludur").min(5,"ürün açıklaması en az 5 karakter olmalıdır").max(300),
+        price: number().required().min(0),
+        stock: number().required().min(0).integer(),
+
+    });
+
 
     return (
         <div className="container mt-5">
             <Formik initialValues={initialValues} onSubmit={values => {
                 console.log(values);
-            }}>
+            }}
+                validationSchema={validationSchema}
+            >
                 <Form>
                     <div className="mb-3">
                         <label className="form-label">Ürün Adı</label>
                         <Field name="title" type="text" className="form-control" />
+                        <ErrorMessage name="title">
+                            {(message) => <span className='text-danger'>{message}</span>}
+                        </ErrorMessage>
                     </div>
                     <div className="mb-3">
                         <label className="form-label">Ürün açıklaması</label>
+                      
                         <Field
                             name="description"
                             type="text"
                             className="form-control"
                         />
+                          <ErrorMessage name="description">
+                            {(message) => <span className='text-danger'>{message}</span>}
+                        </ErrorMessage>
                     </div>
                     <div className="mb-3">
                         <label className="form-label">Ürün Fiyatı</label>
@@ -40,6 +58,9 @@ const AddProduct = (props: Props) => {
                             type="number"
                             className="form-control"
                         />
+                        <ErrorMessage name="price">
+                            {(message) => <span className='text-danger'>{message}</span>}
+                        </ErrorMessage>
                     </div>
                     <div className="mb-3">
                         <label className="form-label">Ürün Stok</label>
@@ -48,6 +69,9 @@ const AddProduct = (props: Props) => {
                             type="number"
                             className="form-control"
                         />
+                        <ErrorMessage name="stock">
+                            {(message) => <span className='text-danger'>{message}</span>}
+                        </ErrorMessage>
                     </div>
                     <button type="submit" className="btn btn-primary">
                         kaydet
