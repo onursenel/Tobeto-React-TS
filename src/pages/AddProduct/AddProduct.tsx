420867df1b093f0f1cf790411575c074961cf676
Onur Şenel <onrsenel@gmail.com>
2024-01-02 15:42:50 +0300

    FormikInput ile kod tekrarından kurtulma

--- src/pages/AddProduct/AddProduct.tsx
+++ src/pages/AddProduct/AddProduct.tsx
@@ -1,6 +1,7 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik'
 
 import { number, object, string } from 'yup';
+import FormikInput from '../../components/FormikInput/FormikInput';
 
 type Props = {};
 
@@ -32,47 +33,11 @@ const AddProduct = (props: Props) => {
                 validationSchema={validationSchema}
             >
                 <Form>
-                    <div className="mb-3">
-                        <label className="form-label">Ürün Adı</label>
-                        <Field name="title" type="text" className="form-control" />
-                        <ErrorMessage name="title">
-                            {(message) => <span className='text-danger'>{message}</span>}
-                        </ErrorMessage>
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Ürün açıklaması</label>
-                      
-                        <Field
-                            name="description"
-                            type="text"
-                            className="form-control"
-                        />
-                          <ErrorMessage name="description">
-                            {(message) => <span className='text-danger'>{message}</span>}
-                        </ErrorMessage>
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Ürün Fiyatı</label>
-                        <Field
-                            name='price'
-                            type="number"
-                            className="form-control"
-                        />
-                        <ErrorMessage name="price">
-                            {(message) => <span className='text-danger'>{message}</span>}
-                        </ErrorMessage>
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Ürün Stok</label>
-                        <Field
-                            name="stock"
-                            type="number"
-                            className="form-control"
-                        />
-                        <ErrorMessage name="stock">
-                            {(message) => <span className='text-danger'>{message}</span>}
-                        </ErrorMessage>
-                    </div>
+                    <FormikInput name="title" label="Ürün Adı"/>
+                    <FormikInput name="description" label="Ürün açıklaması"/>
+                    <FormikInput name="price" label="Ürün Fiyatı"/>
+                    <FormikInput name="stock" label="Ürün stok"/>
+                
                     <button type="submit" className="btn btn-primary">
                         kaydet
                     </button>
